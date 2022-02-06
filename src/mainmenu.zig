1ab9a47f1269df04841b9151d714c4a77a868757
Murilo Baldi <mb@murilobaldi.com>
2022-02-06 11:53:46 +0100

    Add Hash map based menu (no error handling)

---
+++ src/mainmenu.zig
@@ -0,0 +1,165 @@
+const std = @import("std");
+const mem = std.mem;
+const utils = @import("utils.zig");
+const Tui = @import("tui.zig").Tui;
+const Database = @import("database.zig").Database;
+const dm = @import("datamodels.zig");
+const sync = @import("sync.zig");
+const Element = dm.Element;
+const Target = dm.Target;
+const Profile = dm.Profile;
+const stdout = std.io.getStdOut().writer();
+const AutoHashMap = std.hash_map.AutoHashMap;
+
+const Tuple = struct {
+    const Self = @This();
+    string: []const u8,
+    function: fn (*MainMenu) anyerror!void,
+
+    pub fn new(str: []const u8, func: fn (*MainMenu) anyerror!void) Self {
+        return Self{
+            .string = str,
+            .function = func,
+        };
+    }
+};
+
+pub const MainMenu = struct {
+    const Self = @This();
+    options: AutoHashMap(u64, Tuple),
+    should_quit: bool,
+    tui: *Tui,
+    db: *Database,
+    allocator: *const mem.Allocator,
+
+    pub fn init(allocator: *const mem.Allocator, tui: *Tui, db: *Database) !Self {
+        return Self{
+            .options = try getMainMenuOptions(allocator),
+            .tui = tui,
+            .db = db,
+            .should_quit = false,
+            .allocator = allocator,
+        };
+    }
+
+    pub fn run(self: *Self) !void {
+        var opt: u64 = 0;
+        while (!self.should_quit) {
+            try self.printMainMenu();
+
+            opt = utils.readU64(self.allocator, "") catch 0;
+
+            _ = try self.options.get(opt).?.function(self);
+        }
+    }
+
+    fn printMainMenu(self: *Self) !void {
+        var n_items = self.options.count();
+        try stdout.print("\n", .{});
+        var i: u64 = 1;
+        while (i <= n_items) : (i += 1) {
+            var tuple = self.options.get(i).?;
+            try stdout.print("{d}: {s}\n", .{ i, tuple.string });
+        }
+    }
+
+    fn getMainMenuOptions(allocator: *const mem.Allocator) !AutoHashMap(u64, Tuple) {
+        var hash_map = AutoHashMap(u64, Tuple).init(allocator.*);
+
+        try hash_map.put(1, Tuple.new("Add profile"[0..], addProfile));
+        try hash_map.put(2, Tuple.new("Add element"[0..], addElement));
+        try hash_map.put(3, Tuple.new("Add target"[0..], addTarget));
+        try hash_map.put(4, Tuple.new("Link element"[0..], linkElement));
+        try hash_map.put(5, Tuple.new("Sync profile"[0..], syncProfile));
+        try hash_map.put(6, Tuple.new("Restore profile"[0..], restoreProfile));
+        try hash_map.put(7, Tuple.new("Quit"[0..], quit));
+
+        return hash_map;
+    }
+};
+
+pub fn addProfile(menu: *MainMenu) !void {
+    var prof: Profile = undefined;
+    prof = menu.tui.readProfile() catch unreachable;
+    menu.db.insert(prof) catch unreachable;
+}
+
+pub fn addElement(menu: *MainMenu) !void {
+    var element: Element = undefined;
+    element = try menu.tui.readElement();
+    try menu.db.insert(element);
+}
+
+pub fn addTarget(menu: *MainMenu) !void {
+    var target: Target = undefined;
+    target = try menu.tui.readTarget();
+    try menu.db.insert(target);
+}
+
+pub fn linkElement(menu: *MainMenu) !void {
+    var p_idx: usize = undefined;
+    var e_idx: usize = undefined;
+
+    var elements = try menu.db.selectUnlinkedElements();
+    var profiles = try menu.db.selectProfileNames();
+
+    e_idx = menu.tui.selectElement("Select the element to link:\n"[0..], elements) catch {
+        try stdout.print("There are no elements to choose\n", .{});
+        return;
+    };
+    p_idx = menu.tui.selectProfile("Select the profile to link to:\n"[0..], profiles) catch {
+        try stdout.print("There are no profiles to choose\n", .{});
+        return;
+    };
+
+    var element = elements[e_idx];
+    var profile = profiles[p_idx];
+
+    try menu.db.linkElement(element, profile);
+}
+
+pub fn syncProfile(menu: *MainMenu) !void {
+    var p_idx: usize = undefined;
+    var t_idx: usize = undefined;
+
+    var profiles = try menu.db.selectProfileNames();
+
+    p_idx = menu.tui.selectProfile("Choose the profile to sync:\n"[0..], profiles) catch {
+        try stdout.print("There are no profiles to choose\n", .{});
+        return;
+    };
+
+    var profile_name = profiles[p_idx].name;
+    var targets = try menu.db.selectTargets();
+    t_idx = menu.tui.selectTarget("Choose target to sync:\n"[0..], targets) catch {
+        try stdout.print("There are no targets to choose\n", .{});
+        return;
+    };
+    var full_profile = try menu.db.selectProfile(profile_name);
+    try sync.syncProfileToTarget(menu.db.allocator, full_profile, targets[t_idx]);
+}
+
+pub fn restoreProfile(menu: *MainMenu) !void {
+    var p_idx: usize = undefined;
+    var t_idx: usize = undefined;
+
+    var profiles = try menu.db.selectProfileNames();
+
+    p_idx = menu.tui.selectProfile("Choose the profile to restore:\n"[0..], profiles) catch {
+        try stdout.print("There are no profiles to choose\n", .{});
+        return;
+    };
+
+    var profile_name = profiles[p_idx].name;
+    var targets = try menu.db.selectTargets();
+    t_idx = menu.tui.selectTarget("Choose target to restore from:\n"[0..], targets) catch {
+        try stdout.print("There are no targets to choose\n", .{});
+        return;
+    };
+    var full_profile = try menu.db.selectProfile(profile_name);
+    try sync.restoreProfileFromTarget(menu.db.allocator, full_profile, targets[t_idx]);
+}
+
+pub fn quit(menu: *MainMenu) !void {
+    menu.should_quit = true;
+}
--- src/tui.zig
+++ src/tui.zig
@@ -3,14 +3,15 @@ const c = @import("c.zig");
 const dm = @import("datamodels.zig");
 const utils = @import("utils.zig");
 const mem = std.mem;
+const mm = @import("mainmenu.zig");
+const MainMenu = mm.MainMenu;
 const Element = dm.Element;
 const Profile = dm.Profile;
 const Target = dm.Target;
 const ArrayList = std.ArrayList;
 const stdout = std.io.getStdOut().writer();
 const stdin = std.io.getStdIn().reader();
-
-const max_alloc_size = 1_000;
+const Database = @import("database.zig").Database;
 
 const UiError = error{
     InvalidInput,
@@ -21,22 +22,22 @@ pub const Tui = struct {
     const Self = @This();
     allocator: *const mem.Allocator,
 
-    pub fn init(allocator: *const mem.Allocator) !Self {
-        return Self{
+    pub fn init(allocator: *const mem.Allocator) !Tui {
+        return Tui{
             .allocator = allocator,
         };
     }
 
-    pub fn deinit(_: *const Self) void {
+    pub fn deinit(_: *Self) void {
         //
     }
 
-    pub fn readTarget(self: *const Self) !Target {
+    pub fn readTarget(self: *Self) !Target {
         var target: Target = undefined;
 
         target.name = try self.readNotEmpty("Enter the name of the target: ");
-        target.address = try self.readLine("Enter the address of the target: ");
-        target.user = try self.readLine("Enter the user to connect on the target: ");
+        target.address = try utils.readLine(self.allocator, "Enter the address of the target: ");
+        target.user = try utils.readLine(self.allocator, "Enter the user to connect on the target: ");
 
         // Append trailing forward slash to avoid problems with rsync
         target.path = try self.readNotEmpty("Enter the path of the target: ");
@@ -45,14 +46,14 @@ pub const Tui = struct {
         return target;
     }
 
-    pub fn readElement(self: *const Self) !Element {
+    pub fn readElement(self: *Self) !Element {
         var element: Element = undefined;
 
         element.name = try self.readNotEmpty("Enter the name of the element: ");
         element.source = try self.readNotEmpty("Enter the source path of the element: ");
         element.source = try self.maybeAppendForwardSlash(element.source);
 
-        element.destination = try self.readLine("Enter the destination path of the element\n(If empty, will be considered basename(source)): ");
+        element.destination = try utils.readLine(self.allocator, "Enter the destination path of the element\n(If empty, will be considered basename(source)): ");
         if (element.destination.len == 0) {
             element.destination = try self.basename(element.source);
         }
@@ -61,7 +62,7 @@ pub const Tui = struct {
         return element;
     }
 
-    pub fn readProfile(self: *const Self) !Profile {
+    pub fn readProfile(self: *Self) !Profile {
         var profile: Profile = undefined;
 
         profile.name = try self.readNotEmpty("Enter the name of the profile: ");
@@ -69,7 +70,7 @@ pub const Tui = struct {
         return profile;
     }
 
-    pub fn selectProfile(self: *const Self, prompt: []const u8, profiles: []Profile) !usize {
+    pub fn selectProfile(self: *Self, prompt: []const u8, profiles: []Profile) !usize {
         if (profiles.len == 0) {
             return UiError.NoData;
         }
@@ -77,7 +78,7 @@ pub const Tui = struct {
         return self.readOption(prompt, profiles);
     }
 
-    pub fn selectElement(self: *const Self, prompt: []const u8, elements: []Element) !usize {
+    pub fn selectElement(self: *Self, prompt: []const u8, elements: []Element) !usize {
         if (elements.len == 0) {
             return UiError.NoData;
         }
@@ -85,7 +86,7 @@ pub const Tui = struct {
         return self.readOption(prompt, elements);
     }
 
-    pub fn selectTarget(self: *const Self, prompt: []const u8, targets: []Target) !usize {
+    pub fn selectTarget(self: *Self, prompt: []const u8, targets: []Target) !usize {
         if (targets.len == 0) {
             return UiError.NoData;
         }
@@ -93,23 +94,12 @@ pub const Tui = struct {
         return self.readOption(prompt, targets);
     }
 
-    pub fn readU64(self: *const Self, prompt: []const u8) !u64 {
-        return try utils.parseU64(try self.readLine(prompt), 10);
-    }
-
-    fn readLine(self: *const Self, prompt: []const u8) ![]u8 {
-        if (prompt.len > 0) {
-            try stdout.print("{s}", .{prompt});
-        }
-        return (try stdin.readUntilDelimiterOrEofAlloc(self.allocator.*, '\n', max_alloc_size)).?;
-    }
-
-    fn readNotEmpty(self: *const Self, prompt: []const u8) ![]u8 {
+    fn readNotEmpty(self: *Self, prompt: []const u8) ![]u8 {
         var valid: bool = false;
         var str: []u8 = undefined;
 
         while (!valid) {
-            str = try self.readLine(prompt);
+            str = try utils.readLine(self.allocator, prompt);
             if (str.len != 0) {
                 valid = true;
             } else {
@@ -119,7 +109,7 @@ pub const Tui = struct {
         return str;
     }
 
-    fn maybeAppendForwardSlash(self: *const Self, path: []u8) ![]u8 {
+    fn maybeAppendForwardSlash(self: *Self, path: []u8) ![]u8 {
         if (path[path.len - 1] == '/') {
             return path;
         }
@@ -133,7 +123,7 @@ pub const Tui = struct {
         return list.toOwnedSlice();
     }
 
-    fn readOption(self: *const Self, prompt: []const u8, data_slice: anytype) !usize {
+    fn readOption(self: *Self, prompt: []const u8, data_slice: anytype) !usize {
         var valid = false;
         var n = data_slice.len;
         var opt: u64 = undefined;
@@ -147,7 +137,7 @@ pub const Tui = struct {
                 try stdout.print("{d}: {s}\n", .{ i + 1, data_slice[i].name });
             }
 
-            opt = utils.parseU64(try self.readLine(""), 10) catch n + 1;
+            opt = utils.parseU64(try utils.readLine(self.allocator, ""), 10) catch n + 1;
 
             if (opt > 0 and opt <= n) {
                 valid = true;
@@ -158,7 +148,7 @@ pub const Tui = struct {
         return @as(usize, opt - 1);
     }
 
-    fn basename(self: *const Self, path: []u8) ![]u8 {
+    fn basename(self: *Self, path: []u8) ![]u8 {
         var list = ArrayList(u8).init(self.allocator.*);
         defer list.deinit();
 
