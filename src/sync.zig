210c433e774deaad4f8591c92112ed53abe8c188
Murilo Baldi <mb@murilobaldi.com>
2022-03-04 21:39:47 +0100

    Add password for restore command

--- src/sync.zig
+++ src/sync.zig
@@ -45,38 +45,32 @@ pub fn getSyncSrcAndDstString(allocator: *const mem.Allocator, profile_name: []u
 
 pub fn getRestoreSrcAndDstString(allocator: *const mem.Allocator, profile: Profile, target: Target) ![]u8 {
     // rsync -azhvP {t.user}@{t.address}:{t.path}{t.name} /tmp/sorno/
-    var cmd_base = "rsync -azhvP ";
+    var cmd_base = "rsync -sazhvP ";
     var tmp_dir = "/tmp/sorno/";
     var list = ArrayList(u8).init(allocator.*);
     defer list.deinit();
 
     try list.appendSlice(cmd_base);
+    try list.append('"');
     try list.appendSlice(target.user);
     try list.append('@');
     try list.appendSlice(target.address);
     try list.append(':');
     try list.appendSlice(target.path);
     try list.appendSlice(profile.name);
+    try list.append('"');
     try list.append(' ');
+    try list.append('"');
     try list.appendSlice(tmp_dir);
+    try list.append('"');
 
     return list.toOwnedSlice();
 }
 
 pub fn syncProfileToTarget(allocator: *const mem.Allocator, profile: Profile, target: Target, password: []u8) !void {
-    var sshpass = "sshpass -p ";
     var cmd_base = "rsync -sazhvP ";
 
-    var sshpass_list = ArrayList(u8).init(allocator.*);
-    defer sshpass_list.deinit();
-    try sshpass_list.appendSlice(sshpass);
-    try sshpass_list.append('"');
-    try sshpass_list.appendSlice(password);
-    try sshpass_list.append('"');
-    try sshpass_list.append(' ');
-
-    var sshpass_cmd = sshpass_list.toOwnedSlice();
-
+    var sshpass_cmd = try getSshpassCmd(allocator, password);
     for (profile.elements) |element| {
         var list = ArrayList(u8).init(allocator.*);
         defer list.deinit();
@@ -103,27 +97,57 @@ pub fn getRestoreCopyCmd(allocator: *const mem.Allocator, profile_name: []u8, el
     var tmp_dir = "/tmp/sorno/";
 
     try list.appendSlice(base);
+
+    try list.append('"');
     try list.appendSlice(tmp_dir);
     try list.appendSlice(profile_name);
     try list.append('/');
     try list.appendSlice(element.destination);
+    try list.append('"');
     try list.append(' ');
-    try list.appendSlice(element.source);
+    try list.append('"');
+
+    try list.appendSlice("/tmp/sorno/test/");
+    //try list.appendSlice(element.source);
+    try list.append('"');
 
     return list.toOwnedSlice();
 }
 
-pub fn restoreProfileFromTarget(allocator: *const mem.Allocator, profile: Profile, target: Target) !void {
+pub fn restoreProfileFromTarget(allocator: *const mem.Allocator, profile: Profile, target: Target, password: []u8) !void {
     // TODO: There should be a safe way to create the destination directory and copy stuff there
     // without having same name subdirectory, like '/path/directory/directory/content
     var restore_cmd = try getRestoreSrcAndDstString(allocator, profile, target);
+    var sshpass_cmd = try getSshpassCmd(allocator, password);
+
+    var list = ArrayList(u8).init(allocator.*);
+    defer list.deinit();
+
+    try list.appendSlice(sshpass_cmd);
+    try list.append(' ');
+    try list.appendSlice(restore_cmd);
+    var full_cmd = list.toOwnedSlice();
 
     try stdout.print("\nRunning {s}\n", .{restore_cmd});
-    _ = c.system(try cstr.addNullByte(allocator.*, restore_cmd));
+    _ = c.system(try cstr.addNullByte(allocator.*, full_cmd));
 
     for (profile.elements) |element| {
         var copy_cmd = try getRestoreCopyCmd(allocator, profile.name, element);
         std.debug.print("\nRunning {s}\n", .{copy_cmd});
         _ = c.system(try cstr.addNullByte(allocator.*, copy_cmd));
     }
+}
+
+fn getSshpassCmd(allocator: *const mem.Allocator, password: []u8) ![]u8 {
+    var sshpass = "sshpass -p ";
+
+    var list = ArrayList(u8).init(allocator.*);
+    defer list.deinit();
+    try list.appendSlice(sshpass);
+    try list.append('"');
+    try list.appendSlice(password);
+    try list.append('"');
+    try list.append(' ');
+
+    return list.toOwnedSlice();
 }
